cfaf6c94c06c56b106eb733ce8fe2555eede5704
Yash Kumar Sugandhi <85982995+ykstech@users.noreply.github.com>
2024-03-23 23:01:35 +0530

    yash2

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} and iam yash sugandhi
+          count is {count} and iam yash bhai ji sugandhi
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
